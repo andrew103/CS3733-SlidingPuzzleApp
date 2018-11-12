3af15525be0c95e14fd97fe987b109561855c7f1
Andrew Euredjian <andrew_103@rocketmail.com>
2018-11-12 18:04:42 -0500

    Make controller for resetting board

--- src/controller/ResetPuzzleController.java
+++ src/controller/ResetPuzzleController.java
@@ -1,6 +1,11 @@
 package controller;
 
 import puzzle.Puzzle;
+
+import java.awt.Color;
+
+import javax.swing.border.LineBorder;
+
 import model.PuzzleModel;
 
 public class ResetPuzzleController {
@@ -13,6 +18,27 @@ public class ResetPuzzleController {
 	}
 	
 	public void reset() {
-
+		this.model.resetPuzzle();
+		
+		this.app.pieces[0].setLocation(105, 5);
+		this.app.pieces[0].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[1].setLocation(105, 205);
+		this.app.pieces[1].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[2].setLocation(5, 5);
+		this.app.pieces[2].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[3].setLocation(305, 5);
+		this.app.pieces[3].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[4].setLocation(5, 205);
+		this.app.pieces[4].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[5].setLocation(305, 205);
+		this.app.pieces[5].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[6].setLocation(105, 305);
+		this.app.pieces[6].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[7].setLocation(205, 305);
+		this.app.pieces[7].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[8].setLocation(5, 405);
+		this.app.pieces[8].setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		this.app.pieces[9].setLocation(305, 405);
+		this.app.pieces[9].setBorder(new LineBorder(new Color(0, 0, 0), 2));
 	}
 }
