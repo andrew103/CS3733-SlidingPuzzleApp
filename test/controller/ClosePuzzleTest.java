f2b09f5a2d32e3544b2db5b7a6c5b23446eb75e8
Andrew Euredjian <andrew_103@rocketmail.com>
2018-11-12 20:25:45 -0500

    Make test for app closing controller

--- test/controller/ClosePuzzleTest.java
+++ test/controller/ClosePuzzleTest.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class ClosePuzzleTest {
+import junit.framework.TestCase;
+import model.PuzzleModel;
+import puzzle.Puzzle;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClosePuzzleTest extends TestCase {
+	
+	public void testCloseApp() {
+		PuzzleModel m = new PuzzleModel();
+		Puzzle app = new Puzzle(m);
+		
+		app.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				app.setVisible(false);
+				app.dispose();
+			}
+		});
+		
+		ClosePuzzleController controller = new ClosePuzzleController(app, m);
+		controller.close();
+		
+		assertFalse(app.isVisible());
+	}
 }
