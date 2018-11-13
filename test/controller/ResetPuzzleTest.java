12192d64eab46474e2dfcdb5904fc54d28e67524
Andrew Euredjian <andrew_103@rocketmail.com>
2018-11-12 20:41:31 -0500

    Make test for puzzle reset controller

--- test/controller/ResetPuzzleTest.java
+++ test/controller/ResetPuzzleTest.java
@@ -1,5 +1,32 @@
 package controller;
 
-public class ResetPuzzleTest {
+import junit.framework.TestCase;
+import model.PuzzleModel;
+import puzzle.Puzzle;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ResetPuzzleTest extends TestCase {
+
+	public void testAppReset() {
+		PuzzleModel m = new PuzzleModel();
+		Puzzle app = new Puzzle(m);
+		
+		app.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				app.setVisible(false);
+				app.dispose();
+			}
+		});
+
+		app.pieces[9].setLocation(205, 405);
+		m.moveLeft(3, 4);
+		
+		ResetPuzzleController controller = new ResetPuzzleController(app, m);
+		controller.reset();
+		
+		assertEquals(app.pieces[9].getX(), 305);
+		assertEquals(m.getGrid()[4][3], "panel_9");
+	}
 }
