be3f5f512ee1d4c743e8f1d2bef110d7cfb603a8
Andrew Euredjian <andrew_103@rocketmail.com>
2018-11-12 20:57:34 -0500

    Make test for select piece controller

--- test/controller/SelectPieceTest.java
+++ test/controller/SelectPieceTest.java
@@ -1,5 +1,51 @@
 package controller;
 
-public class SelectPieceTest {
+import junit.framework.TestCase;
+import model.PuzzleModel;
+import puzzle.Puzzle;
 
+import java.awt.Color;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.border.LineBorder;
+
+public class SelectPieceTest extends TestCase {
+
+	public void testSelectPiece() {
+		PuzzleModel m = new PuzzleModel();
+		Puzzle app = new Puzzle(m);
+		
+		app.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				app.setVisible(false);
+				app.dispose();
+			}
+		});
+
+		SelectPieceController controller = new SelectPieceController(app, m);
+		controller.selectPiece(250, 150);
+		
+		assertEquals(m.selected_piece, "target");
+	}
+	
+	public void testDeselectAndSelectPiece() {
+		PuzzleModel m = new PuzzleModel();
+		Puzzle app = new Puzzle(m);
+		
+		app.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				app.setVisible(false);
+				app.dispose();
+			}
+		});
+
+		SelectPieceController controller = new SelectPieceController(app, m);
+
+		controller.selectPiece(250, 150);	
+		assertEquals(m.selected_piece, "target");
+
+		controller.selectPiece(250, 250);
+		assertEquals(m.selected_piece, "panel_1");
+	}
 }
